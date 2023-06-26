cd7ee3c688ada66302331f1225687dc6fded00d2
SonjaLadenstein <sonja.ladenstein@gmx.at>
2023-06-26 19:30:19 +0300

    day III part II solution

--- src/AoC/Day3/DayIII.cs
+++ src/AoC/Day3/DayIII.cs
@@ -8,7 +8,8 @@ namespace Day3
 {
     internal class SolutionPartI
     {
-        static readonly string textFile = @"C:\Users\30697\source\repos\AoC\AoC\Day3\input.txt";
+        static readonly string textFile = @"Day3/input.txt";
+
         public int solve()
         {
             int length = 12;
@@ -29,12 +30,12 @@ namespace Day3
                 {
                     sumDigits[i] += line[i] - '0';
                 }
-                
+
             }
 
             for(int i=0; i<length; ++i)
             {
-                gammaRate = sumDigits[i] > total / 2 ? 1 : 0;  
+                gammaRate = sumDigits[i] > total / 2 ? 1 : 0;
 
                 gammaDecimal += gammaRate * Math.Pow(2, length-1-i);
                 epsilonDecimal += ( 1 - gammaRate ) * Math.Pow(2, length-1-i);
@@ -42,5 +43,81 @@ namespace Day3
 
             return (int)(gammaDecimal * epsilonDecimal);
         }
+
     }
+
+
+    internal class SolutionPartII
+    {
+      static readonly string textFile = @"Day3/input.txt";
+
+      public int calculateRating(string[] lines, bool mostCommon){
+        int index;
+        int count;
+        double rating = 0.0;
+        char winningSymbol;
+        int totalNewLines;
+        int zeroCounts;
+
+        HashSet<string> filterLines = new HashSet<string>();
+
+        foreach(string line in lines){
+          filterLines.Add(line);
+        }
+
+        int totalLines = lines.Length;
+        index = 0;
+
+        while(filterLines.Count() > 1){
+          count = 0;
+          zeroCounts = 0;
+          totalNewLines = 0;
+
+          for(int i=0; i<totalLines; ++i){
+            if(!filterLines.Contains(lines[i])) continue;
+            if(lines[i][index] == '1') count++;
+            totalNewLines++;
+          }
+
+          zeroCounts = totalNewLines - count;
+
+          if(mostCommon){
+            winningSymbol = count >= zeroCounts ? '1': '0';
+          }
+          else{
+            winningSymbol = count >= zeroCounts ? '0': '1';
+          }
+
+          for(int i=0; i<totalLines; ++i){
+            if(filterLines.Contains(lines[i]) && lines[i][index] != winningSymbol){
+              filterLines.Remove(lines[i]);
+            }
+          }
+          index++;
+      }
+
+          string survivor = filterLines.First().ToString();
+
+          for(int i=0; i<survivor.Length; ++i){
+            rating += Math.Pow(2, i) * ((double) survivor[survivor.Length - i - 1] - 48);
+          }
+
+          return (int) rating;
+      }
+
+      public int solve(){
+        string[] lines = File.ReadAllLines(textFile);
+        int oxygenRating = calculateRating(lines, true);
+        Console.WriteLine($"Oxygen Rating: {oxygenRating}");
+
+        int C02Rating = calculateRating(lines, false);
+        Console.WriteLine($"C02Rating {C02Rating}.");
+
+        int LifeSupportRating = C02Rating * oxygenRating;
+        Console.WriteLine($"Life Support Rating: {LifeSupportRating}.");
+
+      return LifeSupportRating;
+    }
+
+  }
 }
